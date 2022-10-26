3d47611db2a2d5b04028daa7e79ebe5bb07d67a7
quirkles <alexquirk@joinsherpa.com>
2022-10-26 09:31:38 -0400

    update config

--- src/datasource/index.ts
+++ src/datasource/index.ts
@@ -11,22 +11,23 @@ const {
 
 let connectionOptions: DataSourceOptions = {
   type: 'mongodb',
-  host: DATABASE_HOST,
-  username: DATABASE_USERNAME,
-  password: DATABASE_PASSWORD,
-  database: DATABASE_NAME,
   useUnifiedTopology: true,
   entities: [UserModel]
 }
 if (appConfig.env === 'local') {
   connectionOptions = {
     ...connectionOptions,
+    host: DATABASE_HOST,
+    username: DATABASE_USERNAME,
+    password: DATABASE_PASSWORD,
+    database: DATABASE_NAME,
     port: 27017,
     authSource: 'admin'
   }
 } else {
   connectionOptions = {
     ...connectionOptions,
+    url: `mongodb+srv://${DATABASE_USERNAME}:${DATABASE_PASSWORD}@${DATABASE_HOST}/${DATABASE_NAME}?retryWrites=true&w=majority`,
     ssl: true
   }
 }
