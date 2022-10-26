0c56298675b286acca294ff5362c495779cc670a
quirkles <alexquirk@joinsherpa.com>
2022-10-26 09:31:26 -0400

    update config

--- scripts/decryptSecrets.sh
+++ scripts/decryptSecrets.sh
@@ -1,4 +1,4 @@
-cd ../src/config
+cd ../src/config || exit
 echo "Files found before encryption:"
 ls -a
 find . -type f -name "secrets.*.enc" | sed  -r "s|secrets\.([a-z]+)\.ts\.enc|secrets.\1|g" | xargs -I % sh -c "gcloud kms decrypt --key=secrets-enc-key --keyring=madrid-reds --location=global --plaintext-file=%.ts --ciphertext-file=%.ts.enc"
