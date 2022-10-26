95faf7b47a3826c8cbc869e7f8ddd9103852cc5d
quirkles <alexquirk@joinsherpa.com>
2022-10-25 20:35:29 -0400

    uningnore config files

--- scripts/decryptSecrets.sh
+++ scripts/decryptSecrets.sh
@@ -1,2 +1,7 @@
-cd ../src/config && find . -type f -name "secrets.*.enc" | sed  -r "s|secrets\.([a-z]+)\.ts\.enc|secrets.\1|g" | xargs -I % sh -c "gcloud kms decrypt --key=secrets-enc-key --keyring=madrid-reds --location=global --plaintext-file=%.ts --ciphertext-file=%.ts.enc"
+cd ../src/config
+echo "Files found before encryption:"
+ls -a
+find . -type f -name "secrets.*.enc" | sed  -r "s|secrets\.([a-z]+)\.ts\.enc|secrets.\1|g" | xargs -I % sh -c "gcloud kms decrypt --key=secrets-enc-key --keyring=madrid-reds --location=global --plaintext-file=%.ts --ciphertext-file=%.ts.enc"
+echo "Files found after encryption:"
+ls -a
 
