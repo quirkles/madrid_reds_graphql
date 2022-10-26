3ef919f630ef21a59cebd55add3d551fc80f1195
quirkles <alexquirk@joinsherpa.com>
2022-10-25 21:42:35 -0400

    try to connect

--- src/datasource/index.ts
+++ src/datasource/index.ts
@@ -1,4 +1,4 @@
-import { DataSource } from 'typeorm'
+import { DataSource, DataSourceOptions } from 'typeorm'
 import { appConfig } from '../config'
 import { UserModel } from '../datalayer/models'
 
@@ -9,13 +9,24 @@ const {
   DATABASE_NAME
 } = appConfig
 
-export const AppDataSource = new DataSource({
+let connectionOptions: DataSourceOptions = {
   type: 'mongodb',
   host: DATABASE_HOST,
-  port: 27017,
   username: DATABASE_USERNAME,
   password: DATABASE_PASSWORD,
   database: DATABASE_NAME,
-  authSource: 'admin',
   entities: [UserModel]
-})
+}
+if (appConfig.env === 'local') {
+  connectionOptions = {
+    ...connectionOptions,
+    port: 27017,
+    authSource: 'admin'
+  }
+} else {
+  connectionOptions = {
+    ...connectionOptions,
+    ssl: true
+  }
+}
+export const AppDataSource = new DataSource(connectionOptions)
