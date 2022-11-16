0d18a4fa570829fe2a46617eaad10179de4f695f
quirkles <alexquirk@joinsherpa.com>
2022-11-16 18:07:59 -0500

    auth test query working

--- scripts/encryptSecrets.sh
+++ scripts/encryptSecrets.sh
@@ -1,3 +1,3 @@
-(
-  cd ../src/config && find . -type f -name "secrets.*.ts" | sed  -r "s|secrets\.([a-z]+)\.ts|secrets.\1|g" | xargs -I % sh -c "gcloud kms encrypt --key=secrets-enc-key --keyring=madrid-reds --location=global --plaintext-file=%.ts --ciphertext-file=%.ts.enc"
-)
+cd ../src/config && find . -type f -name "secrets.*.ts" | sed  -r "s|secrets\.([a-z]+)\.ts|secrets.\1|g" | xargs -I % sh -c "gcloud kms encrypt --key=secrets-enc-key --keyring=madrid-reds --location=global --plaintext-file=%.ts --ciphertext-file=%.ts.enc"
+
+cd ..
